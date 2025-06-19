8e2dd8819f55f4b85fa631b3105318d13cb838ab
Sparsh566 <1774.sparsh@gmail.comgit config --global user.emailgit config --global user.email 1774.sparsh@gmail.com>
2025-06-19 12:00:26 +0530

    first file created

---
+++ first.js
@@ -0,0 +1,6 @@
+let jedi = "Obiwan Kenobi"
+console.log(jedi.replace("Obiwan", "Ben"))
+let role = "The Liberator"
+console.log(jedi.concat(" is also known as ", role,))
+let jedi2 = "    Anakin Skywalker    "
+console.log(jedi2.trim())
