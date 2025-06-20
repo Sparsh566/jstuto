a5eeb0fa97ef7fc22bca9656b7ec9d449ac41bf2
Sparsh566 <1774.sparsh@gmail.comgit config --global user.emailgit config --global user.email 1774.sparsh@gmail.com>
2025-06-20 10:48:27 +0530

    Pushing new code to jstuto repo

---
+++ string.js
@@ -0,0 +1,10 @@
+//what will the following print in js
+console.log("har\"".length)
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+const word = 'fox';
+console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence.`)
+// extract the amount out of this string
+let str = "Please give Rs 10000"    
+let amount = str.slice("Please give Rs ".length)
+console.log(amount)
