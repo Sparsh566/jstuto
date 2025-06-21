ef79263d09ad2770d741e79e3aa66d02616994aa
Sparsh566 <1774.sparsh@gmail.comgit config --global user.emailgit config --global user.email 1774.sparsh@gmail.com>
2025-06-21 10:48:42 +0530

    pushing array

---
+++ array.js
@@ -0,0 +1,12 @@
+let marks = [90, 56, 89, 24, 75, null, false]
+console.log(marks)
+console.log(marks[3])
+marks [7] = 69
+marks [1] = 67
+console.log(marks)
+let b = marks.toString()
+let c = marks.join("_")
+console.log(b)
+console.log(c)
+marks.pop()
+console.log(marks)
